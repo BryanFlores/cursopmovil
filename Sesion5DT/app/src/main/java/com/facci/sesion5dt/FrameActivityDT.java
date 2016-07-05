1a197fffb8bbee897a6a0a208bbc0a5a13ef219c
Diego Toala <ceusagaintp@hotmail.com>
2016-07-05 11:48:35 -0500

    Clase del 5 de Julio con 4B.

---
+++ Sesion5DT/app/src/main/java/com/facci/sesion5dt/FrameActivityDT.java
@@ -0,0 +1,13 @@
+package com.facci.sesion5dt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameActivityDT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_activity_dt);
+    }
+}
